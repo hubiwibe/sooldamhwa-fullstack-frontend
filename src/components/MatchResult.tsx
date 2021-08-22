e73f4c68bd4b802669243f0f973447b289fe31f7
yungi jung <hubiwibe@gmail.com>
2021-08-22 16:55:02 +0900

    💄 MatchResult

--- src/components/MatchResult.tsx
+++ src/components/MatchResult.tsx
@@ -1,7 +1,42 @@
+import { Box, Chip, Paper } from '@material-ui/core';
+import { Face } from '@material-ui/icons';
 import React, { memo } from 'react';
 
-const MatchResult = memo(() => {
-  return <div>MatchResult</div>;
+const MatchResult: React.FC = memo(() => {
+  return (
+    <Box
+      display='flex'
+      flexWrap='wrap'
+      justifyContent='center'
+      alignContent='center'
+    >
+      <Box m={1}>
+        <Paper elevation={5}>
+          <Box
+            display='flex'
+            flexWrap='wrap'
+            maxWidth='160px'
+            maxHeight='200px'
+            overflow='auto'
+            justifyContent='center'
+          >
+            <Box m={1}>
+              <Chip icon={<Face />} label='chip' variant='outlined' />
+            </Box>
+            <Box m={1}>
+              <Chip icon={<Face />} label='chip' variant='outlined' />
+            </Box>
+            <Box m={1}>
+              <Chip icon={<Face />} label='chip' variant='outlined' />
+            </Box>
+            <Box m={1}>
+              <Chip icon={<Face />} label='chip' variant='outlined' />
+            </Box>
+          </Box>
+        </Paper>
+      </Box>
+    </Box>
+  );
 });
 
 export default MatchResult;
